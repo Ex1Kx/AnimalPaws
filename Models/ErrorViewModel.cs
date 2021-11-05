e3969535da84866bc4c7ff8063a8022c0b0870d0
Ex1Kx <thenewestekigoh@gmail.com>
2021-11-05 16:59:55 -0500

    [Ex1] Added The Files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANIMAL__PAWS_BETA_1._0_.NET_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
